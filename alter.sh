79a00c98ddbf45a437c70b110bc14b07c4ce2ac4
Ankit Beniwal <pagalprogrammer@protonmail.com>
2020-09-04 16:53:22 +0530

    :pencil2: Added Informative Comments

--- alter.sh
+++ alter.sh
@@ -1,4 +1,4 @@
-year = 2018
+year = 2018 #Change the year as per your need
 for month in {01..12}
 do
   mkdir $month
@@ -7,18 +7,18 @@ do
   do
     mkdir $day
     cd $day
-    randomHours=$((RANDOM % 12))
+    randomHours=$((RANDOM % 12)) # To add a randomness to the commits
     for ((hour=1; hour <= randomHours; hour++)); do
       echo "$hour on $month/$day/$year" > commit.md
       export GIT_COMMITTER_DATE="$year-$month-$day $hour:00:00"
       export GIT_AUTHOR_DATE="$year-$month-$day $hour:00:00"
       git add commit.md -f
       git commit --date="$year-$month-$day 0$hour:00:00" -m "$hour on $month $day $year"
-    done
+    done # End of Hour Loop
     cd ../
-  done
+  done # End of Day Loop
   cd ../
-done
+done # End of Month Loop
 git push origin master
 git rm -rf 0*
 git rm -rf 1*
